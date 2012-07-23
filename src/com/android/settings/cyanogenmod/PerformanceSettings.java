ec3b95228bdee2d22b8d20afc90b7048a7b7095f
Android Meda <hephappy@gmail.com>
2012-07-23 15:12:36 +0300

    surfaceflinger: Improve dithering method (2/2)
    Some devices have blur effect while opening apps.
    This fix adds an option(2) for "Fix blur effect".
    Dithering is always enabled when persist.sys.use_dithering=2
    
    Change-Id: I5b0b9fd767cd0024f90bbcb952ac53b209151ca7
    
    Conflicts:
    	res/xml/performance_settings.xml
    	src/com/android/settings/cyanogenmod/PerformanceSettings.java

--- src/com/android/settings/cyanogenmod/PerformanceSettings.java
+++ src/com/android/settings/cyanogenmod/PerformanceSettings.java
@@ -22,6 +22,7 @@ import android.os.Bundle;
 import android.os.SystemProperties;
 import android.preference.CheckBoxPreference;
 import android.preference.Preference;
+import android.preference.ListPreference;
 import android.preference.PreferenceScreen;
 
 import com.android.settings.R;
@@ -44,11 +45,15 @@ public class PerformanceSettings extends SettingsPreferenceFragment
 
     private static final String USE_16BPP_ALPHA_PROP = "persist.sys.use_16bpp_alpha";
 
+<<<<<<< HEAD
     private static final String DISABLE_BOOTANIMATION_PREF = "pref_disable_bootanimation";
 
     private static final String DISABLE_BOOTANIMATION_PERSIST_PROP = "persist.sys.nobootanimation";
 
     private CheckBoxPreference mUseDitheringPref;
+=======
+    private ListPreference mUseDitheringPref;
+>>>>>>> efb0660... surfaceflinger: Improve dithering method (2/2)
 
     private CheckBoxPreference mUse16bppAlphaPref;
 
@@ -66,6 +71,7 @@ public class PerformanceSettings extends SettingsPreferenceFragment
 
             PreferenceScreen prefSet = getPreferenceScreen();
 
+<<<<<<< HEAD
             mUseDitheringPref = (CheckBoxPreference) prefSet.findPreference(USE_DITHERING_PREF);
             mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
             mDisableBootanimPref = (CheckBoxPreference) prefSet.findPreference(DISABLE_BOOTANIMATION_PREF);
@@ -73,7 +79,15 @@ public class PerformanceSettings extends SettingsPreferenceFragment
             String useDithering = SystemProperties.get(USE_DITHERING_PERSIST_PROP,
                     USE_DITHERING_DEFAULT);
             mUseDitheringPref.setChecked("1".equals(useDithering));
+=======
+            String useDithering = SystemProperties.get(USE_DITHERING_PERSIST_PROP, USE_DITHERING_DEFAULT);
+            mUseDitheringPref = (ListPreference) prefSet.findPreference(USE_DITHERING_PREF);
+            mUseDitheringPref.setOnPreferenceChangeListener(this);
+            mUseDitheringPref.setValue(useDithering);
+            mUseDitheringPref.setSummary(mUseDitheringPref.getEntry());
+>>>>>>> efb0660... surfaceflinger: Improve dithering method (2/2)
 
+            mUse16bppAlphaPref = (CheckBoxPreference) prefSet.findPreference(USE_16BPP_ALPHA_PREF);
             String use16bppAlpha = SystemProperties.get(USE_16BPP_ALPHA_PROP, "0");
             mUse16bppAlphaPref.setChecked("1".equals(use16bppAlpha));
 
@@ -98,10 +112,7 @@ public class PerformanceSettings extends SettingsPreferenceFragment
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference == mUseDitheringPref) {
-            SystemProperties.set(USE_DITHERING_PERSIST_PROP,
-                    mUseDitheringPref.isChecked() ? "1" : "0");
-        } else if (preference == mUse16bppAlphaPref) {
+        if (preference == mUse16bppAlphaPref) {
             SystemProperties.set(USE_16BPP_ALPHA_PROP,
                     mUse16bppAlphaPref.isChecked() ? "1" : "0");
         } else if (preference == mDisableBootanimPref) {
@@ -116,8 +127,13 @@ public class PerformanceSettings extends SettingsPreferenceFragment
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-
-        return false;
+        if (preference == mUseDitheringPref) {
+            String newVal = (String) newValue;
+            int index = mUseDitheringPref.findIndexOfValue(newVal);
+            SystemProperties.set(USE_DITHERING_PERSIST_PROP, newVal);
+            mUseDitheringPref.setSummary(mUseDitheringPref.getEntries()[index]);
+        }
+        return true;
     }
 
 }
